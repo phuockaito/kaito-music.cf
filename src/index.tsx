ff5553f37842172f8747841b25b9aeccad8c4786
phuockaito <huuphuocit1999@gmail.com>
2023-12-08 17:25:48 +0700

    space

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
