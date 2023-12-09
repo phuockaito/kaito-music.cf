e6b1a2d68feb4a90349195353a94f26badef15dd
phuockaito <huuphuocit1999@gmail.com>
2023-12-09 08:16:24 +0700

    build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./styles/index.css";
 
@@ -12,19 +11,19 @@ import { App } from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { UseContextProvider } from "contexts/use-context-provider";
+import React from "react";
 moment.locale("vi");
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    // <React.StrictMode>
-    <Provider store={store}>
-        <Router>
-            <UseContextProvider>
-                <App />
-                <Analytics />
-                <SpeedInsights />
-            </UseContextProvider>
-        </Router>
-    </Provider>
-    // </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <UseContextProvider>
+                    <App />
+                    <Analytics />
+                </UseContextProvider>
+            </Router>
+        </Provider>
+    </React.StrictMode>
 );
